c89af672efd3cf24ebaca74e076e846c95e9e7bd
mjuferdi <juferdi.mario@yahoo.com>
2018-05-22 20:02:01 +0200

    Convert to MVC design pattern.

---
+++ Toedoe/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Toedoe
+//
+//  Created by Mario Muhammad on 22.05.18.
+//  Copyright © 2018 Mario Muhammad. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var tittle : String = ""
+    var done : Bool = false
+
+}
